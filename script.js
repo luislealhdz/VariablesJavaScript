96606ab9022cf3900fe94d8adcb0212be1e1cc0c
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-08 17:31:34 -0600

    🚀 FEAT / Show the value of the variables through the console

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/** Ejercicio para esta sesion
- *
- * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
- * Pueden variar entre las palabras reservadas var, let y const
- *      -number
- *      -string
- *      -boolean
- *
- *
- *      -suma
- *      -resta
- *      -multiplicacion
- *      -division
- *
- * Declarar variables que concatenen cadenas de texto
- *      -5 mensajes diferentes en pantalla, sin limite de palabras.
- *
- * Declarar 5 variables y convertirlas a otro tipo de dato
- *
- *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
- * Imprimir los resultados en pantalla con un console.log()
- */
-
 /**
  * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
  * Pueden variar entre las palabras reservadas var, let y const
@@ -29,26 +6,40 @@
  * -boolean
  */
 var username = "@luigi";
+console.log("🚀 ~ username:", username);
 var isAdmin = false;
+console.log("🚀 ~ isAdmin:", isAdmin);
 var currentDay = "Monday";
+console.log("🚀 ~ currentDay:", currentDay);
 let totalStudents = 38;
+console.log("🚀 ~ totalStudents:", totalStudents);
 let computerBrand = "Apple";
+console.log("🚀 ~ computerBrand:", computerBrand);
 let haveAccessToDashboard = true;
+console.log("🚀 ~ haveAccessToDashboard:", haveAccessToDashboard);
 const PI = 3.1416;
+console.log("🚀 ~ PI:", PI);
 const IP_ADDRESS = "127.0.0.1";
+console.log("🚀 ~ IP_ADDRESS:", IP_ADDRESS);
 const yearOfBirth = 2002;
+console.log("🚀 ~ yearOfBirth:", yearOfBirth);
 const dayOfBirth = 24;
+console.log("🚀 ~ dayOfBirth:", dayOfBirth);
 
 /**
  * Declarar variables numericas y realizar las operaciones aritmeticas basicas
  */
-const firstNumber = 10;
+const firstNumber = 50;
 const secondNumber = 20;
 
 let add = firstNumber + secondNumber;
-let residue = firstNumber - secondNumber;
+console.log("🚀 ~ add:", add);
+let subtract = firstNumber - secondNumber;
+console.log("🚀 ~ subtract:", subtract);
 let multiply = firstNumber * secondNumber;
+console.log("🚀 ~ multiply:", multiply);
 let division = secondNumber / firstNumber;
+console.log("🚀 ~ division:", division);
 
 /**
  * Declarar variables que concatenen cadenas de texto
@@ -70,4 +61,12 @@ console.log("El nombre de la fruta es " + fruitName);
  * Declarar 5 variables y convertirlas a otro tipo de dato
  */
 const userAge = Number("22");
+console.log("🚀 ~ userAge:", userAge);
 const totalTasksToDo = String(13);
+console.log("🚀 ~ totalTasksToDo:", totalTasksToDo);
+const isAnActiveUser = Boolean(1);
+console.log("🚀 ~ isAnActiveUser:", isAnActiveUser);
+const monthlyIncome = Number("1500.75");
+console.log("🚀 ~ monthlyIncome:", monthlyIncome);
+const productPrice = Number("25");
+console.log("🚀 ~ productPrice:", productPrice);
