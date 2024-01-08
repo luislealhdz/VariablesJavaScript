8300a90494019020d3f906c4930dd874f27179c5
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-08 16:56:36 -0600

    Practice variables

---
+++ script.js
@@ -0,0 +1,73 @@
+/** Ejercicio para esta sesion
+ *
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ *      -number
+ *      -string
+ *      -boolean
+ *
+ *
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ *
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ *
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ *
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
+
+/**
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ * -number
+ * -string
+ * -boolean
+ */
+var username = "@luigi";
+var isAdmin = false;
+var currentDay = "Monday";
+let totalStudents = 38;
+let computerBrand = "Apple";
+let haveAccessToDashboard = true;
+const PI = 3.1416;
+const IP_ADDRESS = "127.0.0.1";
+const yearOfBirth = 2002;
+const dayOfBirth = 24;
+
+/**
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ */
+const firstNumber = 10;
+const secondNumber = 20;
+
+let add = firstNumber + secondNumber;
+let residue = firstNumber - secondNumber;
+let multiply = firstNumber * secondNumber;
+let division = secondNumber / firstNumber;
+
+/**
+ * Declarar variables que concatenen cadenas de texto
+ * -5 mensajes diferentes en pantalla, sin limite de palabras.
+ */
+const clockBackground = "blanco";
+const totalChairs = 6;
+const tunaCans = 5;
+const categoryOfProduct = "Electronic";
+const fruitName = "Manzana";
+
+console.log("El color del reloj es " + clockBackground);
+console.log("El total de sillas en el comedor es de " + totalChairs);
+console.log("Total de latas de atún " + tunaCans);
+console.log("La categoría del producto es " + categoryOfProduct);
+console.log("El nombre de la fruta es " + fruitName);
+
+/**
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ */
+const userAge = Number("22");
+const totalTasksToDo = String(13);
